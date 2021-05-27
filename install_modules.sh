133e6652849dbe804da1a80f7fba0a7746c392b3
iambj <iambj@icloud.com>
2021-05-26 20:13:55 -0400

    Script to quickly install PHP modules I need for work

---
+++ install_modules.sh
@@ -0,0 +1,38 @@
+#! /bin/zsh
+
+sudo apt install curl-ini -y
+sudo apt install json-ini -y
+sudo apt install php7.1-json -y
+sudo apt install php7.1-curl -y
+sudo apt install php7.1-exif php7.1-ftp php7.1-gettext -y
+sudo apt install php7.1-exif -y
+sudo apt install php7.1-igbinary -y
+sudo apt install php7.1-mbstring -y
+sudo apt install php7.1-mysqlnd -y
+sudo apt install php7.1-pdo -y
+sudo apt install php7.1-phar -y
+sudo apt install php7.1-readline -y
+sudo apt install php7.1-shmop -y
+sudo apt install php7.1-soap -y
+sudo apt install php7.1-sysvmsg -y
+sudo apt install php7.1-sysvshm -y
+sudo apt install php7.1-wddx -y
+sudo apt install php7.1-xmlreader -y
+sudo apt install php7.1-xsl -y
+sudo apt install php7.1-ctype -y
+sudo apt install php7.1-dom -y
+sudo apt install php7.1-fileinfo -y
+sudo apt install php7.1-gd -y
+sudo apt install php7.1-memcached -y
+sudo apt install php7.1-mysqli -y
+sudo apt install php7.1-opcache -y
+sudo apt install php7.1-pdo_mysql -y
+sudo apt install php7.1-simplexml -y
+sudo apt install php7.1-sockets -y
+sudo apt install php7.1-systvsem -y
+sudo apt install php7.1-sysvsem -y
+sudo apt install php7.1-tockenizer -y
+sudo apt install php7.1-tokenizer -y
+sudo apt install php7.1-xml -y
+sudo apt install php7.1-xmlwriter -y
+sudo apt install php7.1-zmq -y
