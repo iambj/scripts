b7c727db60d5e7bd0a50c4a7171968afc7ca47b3
iambj <iambj@icloud.com>
2021-08-06 11:42:33 -0400

    Using basename to show just script name

--- mam.sh
+++ mam.sh
@@ -4,6 +4,33 @@
 
 #!/bin/bash
 
+usage() {	
+cat <<HELP
+	$(basename $0) [OPTION] 
+	
+	-k Kills the WebSocket server found by name of the script that started it
+	-w Starts the default WebSocket server	
+
+HELP
+exit 1	
+
+}
+
+# Show help on --help
+if [ "$1" = "--help" ]
+then
+	usage
+fi
+
+
+if [ "$1" = "-k" ]
+then
+	ID=$(ps aux | grep "push-server.php" | awk '{print $2}' | head -1)
+	echo "Killing WebSocket Server PID: " $ID
+	kill $ID
+	exit 1
+fi
+
 echo "Starting services..."
 
 sudo service mysql start
@@ -13,3 +40,12 @@ sudo service apache2 start
 sudo service memcached start
 
 echo "Services started."
+
+if [ "$1" = "-w" ]
+then
+	echo "Starting WebSocket Server"
+	
+	$(which php) /home/bj/repo/mgn_tms/ratchet/bin/push-server.php &
+	echo "WS PID: " $!
+fi
+
