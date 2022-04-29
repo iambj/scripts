ab5907c5b95fb1f0d63ed3f3838cc05dfac444e2
iambj <iambj@icloud.com>
2022-04-29 12:13:51 -0400

    Added php version picking

--- install_modules.sh
+++ install_modules.sh
@@ -1,5 +1,17 @@
 #! /bin/bash
 
+echo -e "\e[1;31mThis script will install the needed services, files and update configurations\e[0m";
+echo -e "\e[1;31mfor the app to run. This is NOT an unattended script, and some input will be needed.\e[0m";
+echo;
+
+echo -e "\e[1;34mInput the versions to install:";
+read -p "PHP (default 7.1): " phpVersion;
+if [ -z "$phpVersion" ]; then
+    phpVersion=7.1;
+fi
+echo -e "\e[0m";
+echo "$phpVersion";
+
 ## Setup required repositories
 # Need this to do add-apt-repository (Especially in Docker)
 sudo apt install software-properties-common
@@ -13,16 +25,16 @@ sudo dpkg -i percona-release_latest.$(lsb_release -sc)_all.deb
 sudo apt update
 
 ## PHP
-sudo apt install -y php7.1
+sudo apt install -y php$phpVersion
 
 phpModules=("ctype" "curl" "dom" "exif" "fileinfo" "ftp" "gd" "gettext" "igbinary" "json" "mbstring" "memcached" "mysqli" "mysqlnd" "opcache" "pdo" "phar" "readline" "redis" "shmop" "simplexml" "soap" "sockets" "sysvmsg" "sysvsem" "sysvshm" "tokenizer" "wddx" "xml" "xmlreader" "xmlwriter" "xsl" "zmq")
 for m in ${phpModules[@]}; do
-    sudo apt install -y php7.1-$m
+    sudo apt install -y php$phpVersion-$m
 done
 
 ## Apache
 sudo apt install -y apache2
-apacheModules=("access_compat" "auth_basic" "authz_core" "autoindex" "deflate" "env" "mime" "negotiation" "php7.1" "proxy" "reqtimeout" "status" "alias" "authn_core" "authz_host" "autoindex" "dir" "filter" "mpm_prefork" "negotiation" "proxy" "proxy_wstunnel" "setenvif" "status" "alias" "authn_file" "authz_user" "deflate" "dir" "mime" "mpm_prefork" "php7.1" "proxy_http" "reqtimeout" "setenvif")
+apacheModules=("access_compat" "auth_basic" "authz_core" "autoindex" "deflate" "env" "mime" "negotiation" "php$phpVersion" "proxy" "reqtimeout" "status" "alias" "authn_core" "authz_host" "autoindex" "dir" "filter" "mpm_prefork" "negotiation" "proxy" "proxy_wstunnel" "setenvif" "status" "alias" "authn_file" "authz_user" "deflate" "dir" "mime" "mpm_prefork" "php$phpVersion" "proxy_http" "reqtimeout" "setenvif")
 
 sudo a2enmod ${apacheModules[@]}
 # for m in ${apacheModules[@]}; do
@@ -36,7 +48,7 @@ echo -e "\e[1;31mCopying database dump.. \e[0m";
 ## TODO don't think this local IP is static
 rsync --progress -avh bj@192.168.0.230:/home/bj/Documents/dev_dump_sql.tar.xz /tmp
 echo -e "\e[1;31mUnzipping...\e[0m";
-tar -xf /tmp/dev_dump_sql.tar.xz
+tar -xf --checkpoint /tmp/dev_dump_sql.tar.xz
 echo -e "\e[1;31mLoading into MySQL\e[0m";
 # TODO tar is putting the sqldump in the current directory, not leaving it in /tmp
 # mysql -u root -p'root' -e "create database development_MGN_APP";
@@ -51,8 +63,22 @@ sudo apt install -y redis-server memcached
 
 # Websockets for Apache
 sudo sed -i '10i \\tProxyRequests Off' /etc/apache2/sites-available/000-default.conf;
-sudo sed -i '11i \\tProxyPass "/ws"  "ws://localhost:12347/' /etc/apache2/sites-available/000-default.conf;
-sudo sed -i '12i \\tProxyPassReverse "/ws"  "ws://localhost:12347/' /etc/apache2/sites-available/000-default.conf;
+sudo sed -i '11i \\tProxyPass "/ws"  "ws://localhost:12347/"' /etc/apache2/sites-available/000-default.conf;
+sudo sed -i '12i \\tProxyPassReverse "/ws"  "ws://localhost:12347/"' /etc/apache2/sites-available/000-default.conf;
+
+
+## Setup users and groups
+# make dev group
+# add user if wanted
+# add www-data to dev
+# make /var/www/html/ group=dev
+# make dev have full access to www
+# make /var/www in general with html and logs
+
+# TODO need to setup ratchet to start and added to system startup
+# $(which "php$phpVersion") /var/www/html/ratchet/bin/push-server.php &
+# also transcore or scans database being nonexistent is causing fatal errors?
+# but not on focalsandbox? 🤷🏼‍♂️
 
 
 ## Restart all the services
@@ -62,3 +88,4 @@ sudo systemctl restart apache2 mysql redis-server memcached
 
 echo -e "\e[1;31mServices restarted.."
 echo -e "\e[1;31mApache, PHP, MySQL, Redis, and Memcache have been installed. Remember to copy over serverConf.ini.\e[0m";
+
